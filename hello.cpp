22262d35431874fee01324a64311d994a58e6ff3
Vasilsn77 <vasil.s.n777@gmail.com>
2025-03-11 19:47:22 +0530

    Updated print statement in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
+  cout << "Hello, Vasil\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this (edited) \n";
   return 0;
